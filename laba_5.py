59c84784102ce793431e46c94c61f2e4a1aed798
art4iba2 <99066833+art4iba2@users.noreply.github.com>
2022-04-30 18:53:31 +0400

    Update laba_5.py

--- laba_5.py
+++ laba_5.py
@@ -9,99 +9,69 @@
 вычисляется выражение (AТ +G-FТ)*K, где G-нижняя треугольная матрица, полученная из А.
 Выводятся по мере формирования А, F и все матричные операции последовательно.
 """
-
 import time
 import numpy as np
-
-def print_matrix(M, matr_name):
-    print("матрица " + matr_name )
-    for i in M:  # делаем перебор всех строк матрицы
-        for j in i:  # перебираем все элементы в строке
-            print("%5d" % j, end=' ')
-        print()
-
 try :
     N = int(input("Введите количество строк (столбцов) квадратной матрицы больше 3 : "))
     while N < 4 :
-        N = int(input(
-            "Вы ввели неверное число\nВведите количество строк (столбцов) квадратной матрицы больше 3 :"))
+        N = int(input("Вы ввели неверное число\nВведите количество строк (столбцов) квадратной матрицы больше 3 :"))
     K = int(input("Введите число К="))
     start = time.time()
     A = np.zeros((N,N),dtype=int)
     F = np.zeros((N, N), dtype=int)
-
-    for i in range (N):            #задаем матрицу А
+    for i in range (N):     #формируем матрицу А
         for j in range(N):
             A[i][j]=np.random.randint(-10,10)
             #A[i][j] = i*10+j
-
     middle = time.time()
-    print("A time = ", middle - start)
-    print_matrix(A,"A")
-
-    for i in range (N):            #копируем элементы из матрицы А в F
+    print("A time = ", middle - start,"\nmatrix A: \n",A)
+    for i in range (N):      #формируем матрицу F, копируя из матрицы А
         for j in range(N):
-            F[i][j]=A[i][j]
-    print_matrix(F,"F")
-    E=[]
-    n = N // 2
-    E = np.zeros((n,n),dtype=int)
-
-    for i in range (n):          #формируем подматрицу E для проверки условий
+            F[i][j]=A[i][j] 
+    n = N // 2         #размерность подматрицы
+    E = np.zeros((n,n),dtype=int)   #формируем матрицу Е
+    for i in range (n):
         for j in range (n):
             E[i][j]=A[i][j]
-    print ("E time = ", 0)
-    print_matrix(E,"E")
-    enumeration = []
+    print ("E time = ", 0, "\n matrix E:\n",E)
+    enumerationL = []   #массив для чисел из нечетных строк
+    enumerationC = []   #массив для чисел из нечетных столбцов
     for i in range(n):
         for j in range(n):
             if i%2==0 :
-                enumeration.append(E[i][j])
-    maxima=max(enumeration)
-    print(enumeration, "\nMaximum element in lines= ",maxima )     #ищем нечетный элемент в нечетных строках
-
-    enumeration = []
-    for i in range(n):
+                enumerationC.append(E[i][j])
+    for i in range (n):
         for j in range(n):
-            if j%2 == 0:
-                enumeration.append(E[i][j])
-    summ=sum(enumeration)
-    print(enumeration, "\nSum element in columns = ",summ )      #находим сумму элементов в нечетных столбцах
-
+            if j%2==0:
+                enumerationL.append(E[i][j])
+    maxima=max(enumerationL)
+    summ = sum(enumerationC)
+    print(enumerationL, "\nMaximum element in columns= ",maxima , "\n",enumerationC, "\nSum element in lines = ",summ )
     if summ < maxima:
         print("Так как Сумма элементов < максимального элемента => меняем B и С симметрично")
         for i in  range (n):
             for j in range (n):
                 F[i][n+j]=A[N-1-i][n+j]
                 F[N-1-i][n+j]=A[i][n+j]
-
     else:
         print("Так как Сумма элементов > максимального элемента => меняем B и E несимметрично")
         F[0:n ,0:n] = A[0:n ,n+N%2 : N]
         F[0:n ,n+N%2 : N] = A[0:n ,0:n]
     middle2 = time.time()
-    print_matrix(A,"A")
-    print("F new time = ",middle2-middle )
-    print_matrix(F,"F")
-
+    print("matrix A: \n",A , "\n matrix F: \n", F)
     if np.linalg.det(A) == 0 or np.linalg.det(F) == 0:
         print("Матрица A или F вырождена => нельзя вычислить")
     elif np.linalg.det(A) > sum(F.diagonal()):
-
-        print("определитель матрицы А больше суммы диагональных элементов матрицы F")
         A = (A - np.dot(1,np.transpose(A)) - np.dot(K,(F-1)))
         finish = time.time()
-        print("A time = :", finish - middle2)
-        print_matrix(A,"A")
+        print("определитель матрицы А больше суммы диагональных элементов матрицы F","\nA time = :", finish - middle2,"\n",A)
     else:
-        print("определитель матрицы А меньше суммы диагональных элементов матрицы F")
-        print("Треугольная матрица из А :\n",np.tril(A))
-
         A = np.dot(np.transpose(A) + np.tril(A) - np.transpose(F) ,K)
         finish = time.time()
-        print("A time = :", finish - middle2)
-        print_matrix(A,"A")
-
+        print("определитель матрицы А меньше суммы диагональных элементов матрицы F", "\nТреугольная матрица из А :\n",np.tril(A) ,"\nA time = :", finish - middle2, "\n",A,"\n")
+    for i in A:  # делаем перебор всех строк матрицы
+        for j in i:  # перебираем все элементы в строке
+            print("%5d" % j, end=' ')
+        print()
 except ValueError:
     print("\nЭто не число")
-
